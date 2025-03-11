e8468065fd5118d59d8e1e585da454d6fadf1ee6
WilhelmNK <159484526+WilhelmNK@users.noreply.github.com>
2025-03-11 17:05:00 -0300

    dadaada

---
+++ Jogo do Careca(ailton)/Program.cs
@@ -0,0 +1,124 @@
+﻿using System;
+
+namespace JogoDaVelha
+{
+    class Jogo
+    {
+        private char[,] tabuleiro;
+        private char jogadorAtual;
+        private int jogadas;
+
+        public Jogo()
+        {
+            tabuleiro = new char[,]
+            {
+                { '1', '2', '3' },
+                { '4', '5', '6' },
+                { '7', '8', '9' }
+            };
+            jogadorAtual = 'X';
+            jogadas = 0;
+        }
+
+        public void Iniciar()
+        {
+            bool jogoAtivo = true;
+            while (jogoAtivo)
+            {
+                Console.Clear();
+                ExibirTabuleiro();
+                Console.WriteLine($"Jogador {jogadorAtual}, escolha uma posição: ");
+                string entrada = Console.ReadLine();
+
+                if (int.TryParse(entrada, out int posicao) && posicao >= 1 && posicao <= 9)
+                {
+                    if (MarcarPosicao(posicao))
+                    {
+                        jogadas++;
+                        if (VerificarVitoria())
+                        {
+                            Console.Clear();
+                            ExibirTabuleiro();
+                            Console.WriteLine($"Jogador {jogadorAtual} venceu!");
+                            jogoAtivo = false;
+                        }
+                        else if (jogadas == 9)
+                        {
+                            Console.Clear();
+                            ExibirTabuleiro();
+                            Console.WriteLine("Empate!");
+                            jogoAtivo = false;
+                        }
+                        else
+                        {
+                            jogadorAtual = jogadorAtual == 'X' ? 'O' : 'X';
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Posição já ocupada, tente novamente!");
+                        Console.ReadKey();
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Entrada inválida, tente novamente!");
+                    Console.ReadKey();
+                }
+            }
+        }
+
+        private void ExibirTabuleiro()
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    Console.Write(tabuleiro[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        private bool MarcarPosicao(int posicao)
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    if (tabuleiro[i, j] == posicao.ToString()[0])
+                    {
+                        tabuleiro[i, j] = jogadorAtual;
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+
+        private bool VerificarVitoria()
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                if (tabuleiro[i, 0] == jogadorAtual && tabuleiro[i, 1] == jogadorAtual && tabuleiro[i, 2] == jogadorAtual)
+                    return true;
+                if (tabuleiro[0, i] == jogadorAtual && tabuleiro[1, i] == jogadorAtual && tabuleiro[2, i] == jogadorAtual)
+                    return true;
+            }
+            if (tabuleiro[0, 0] == jogadorAtual && tabuleiro[1, 1] == jogadorAtual && tabuleiro[2, 2] == jogadorAtual)
+                return true;
+            if (tabuleiro[0, 2] == jogadorAtual && tabuleiro[1, 1] == jogadorAtual && tabuleiro[2, 0] == jogadorAtual)
+                return true;
+            return false;
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            Jogo jogo = new Jogo();
+            jogo.Iniciar();
+        }
+    }
+}
---
+++ ultimo fabiano peixoto/Program.cs
@@ -0,0 +1,130 @@
+﻿internal class program
+{
+    private static void Main(string[] args)
+    {
+        string mensagem, comando;
+        string jogador;
+        string[,] tabuleiro;
+        bool play;
+        int posicaoX,posicaoY;
+
+
+        mensagem = "fabiano peixoto";
+        comando = "";
+        tabuleiro = new string[10, 10]
+        {
+            // 0   1   2   3   4   5   6   7   8   9
+            { "#","#","#","#","#","#","#","#","#","#" },// 0
+            { "#"," "," "," "," ","?"," "," "," ","#" },// 1
+            { "#"," "," "," "," "," "," "," "," ","#" },// 2
+            { "#"," "," "," "," "," "," "," "," ","#" },// 3
+            { "#"," "," "," "," "," "," "," "," ","#" },// 4
+            { "#"," "," "," "," ","?"," "," "," ","#" },// 5
+            { "#"," "," "," "," "," "," "," "," ","#" },// 6
+            { "#"," "," "," "," "," "," "," "," ","#" },// 7
+            { "#"," "," "," "," "," "," "," "," ","#" },// 8
+            { "#","#","#","#","#","#","#","#","#","#" } // 9
+        };
+        posicaoX = 1;
+        posicaoY = 1;
+        jogador = "0";
+
+
+    GameStart:
+        Console.Clear();
+        Console.WriteLine(mensagem);
+        Console.WriteLine("B = blyat");
+        Console.WriteLine("S = sair");
+        comando = Console.ReadLine();
+
+        if (comando.ToUpper() == "B")
+        {
+            play = true;
+
+            while (play)
+            {
+                tabuleiro[posicaoY, posicaoX] = jogador;
+
+                Console.Clear();
+
+
+                for (int y = 0; y < 10; y++)
+                {
+                    for (int x = 0; x < 10; x++)
+                    {
+                        Console.Write(tabuleiro[y, x]);
+                    }
+                    Console.WriteLine("");
+                }
+                
+                
+                Console.WriteLine("Digite as Teclas para se movimentar");
+                Console.WriteLine("(A) - Esquerda");
+                Console.WriteLine("(D) - Direita");
+                Console.WriteLine("(W) - Cima");
+                Console.WriteLine("(S) - Baixo");
+                comando = Console.ReadLine();
+
+                switch (comando.ToUpper())
+                {
+                    case "A":
+
+                        if(tabuleiro[posicaoY,posicaoX-1] != "#")
+                        {
+                            tabuleiro[posicaoY, posicaoX] = " ";
+                            posicaoX--;
+
+                        }
+                        else if (tabuleiro[posicaoY, posicaoX - 1] == "?")
+                        {
+                            Console.Beep();
+                        }
+                        break;
+                    case "D":
+
+
+                        if (tabuleiro[posicaoY, posicaoX] == "?")
+                        {
+                            Console.Beep();
+                        }
+                        else if (tabuleiro[posicaoY, posicaoX + 1] != "#")
+                        {
+                            tabuleiro[posicaoY, posicaoX] = " ";
+                            posicaoX++;
+                        }
+
+                        break;
+                    case "W":
+                        if (tabuleiro[posicaoX, posicaoY - 1] != "#")
+                        {
+                            tabuleiro[posicaoY, posicaoX] = " ";
+                            posicaoY--;
+                        }
+                        break;
+                    case "S":
+                        if (tabuleiro[posicaoX, posicaoY + 1] != "#")
+                        {
+                            tabuleiro[posicaoY, posicaoX] = " ";
+                            posicaoY++;
+                        }
+                        break;
+                    default:
+                        Console.WriteLine("blyat ");
+                        break;
+                    
+                }
+            }
+        }
+ 
+        else if (comando.ToUpper() == "S")
+        {
+            Console.Beep();
+            Console.WriteLine("nahui");
+            Console.ReadKey();
+        }
+        else {
+            goto GameStart;
+        } 
+
+    }
+}
